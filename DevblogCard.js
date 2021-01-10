f1334fba7db3ae1b2816275066de3cc2aa2c8382
Souravdey777 <piyush.kolkata@gmail.com>
2021-01-10 18:48:04 +0530

    Added Dev blogs API

---
+++ DevblogCard.js
@@ -0,0 +1,166 @@
+const axios = require('axios');
+
+const getBase64 = async (url) => {
+    return await axios
+        .get(url, {
+            responseType: 'arraybuffer'
+        })
+        .then(response => Buffer.from(response.data, 'binary').toString('base64'))
+}
+
+const DblogCardH = async (data) => {
+    const blogImage = await getBase64(data.cover_image);
+    const mediumLogo = await getBase64("https://raw.githubusercontent.com/Souravdey777/Github-Cards-External-Blogs/master/mediumlogo.png");
+    const blogDate = new Date(data.published_at).toLocaleString('default', { year: 'numeric', month: 'short', day: 'numeric' })
+    const blogLink = data.url;
+
+    return `
+  <svg height="320px" width="200">
+    <style>
+      .title {
+        font: 600 10px 'Segoe UI', Ubuntu, Sans-Serif;
+        fill: #2f80ed;
+        animation: fadeInAnimation 0.8s ease-in-out forwards;
+      }
+
+      .smallTitle {
+        font: 600 10px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif; fill: #333;
+      }
+
+      .bold {
+        font - weight: 700 
+      }
+    </style>
+
+    <a href="${blogLink}" target="_blank">
+      <rect
+        data-testid="card-bg"
+        x="0.5"
+        y="0.5"
+        rx="4.5"
+        height="95%"
+        stroke="#E4E2E2"
+        width="95%"
+        fill="#fffefe"
+        stroke-opacity="1"
+      />
+      <rect
+        height="200"
+        stroke="#E4E2E2"
+        width="85%"
+        fill="#EFEFEF" x="10" y="25"
+        stroke-opacity="1" rx="4.5"
+      />
+      <image xlink:href="data:image/png;base64,${blogImage}"  x="10" y="25" width="85%" height="180" />
+
+   <image xlink:href="data:image/png;base64,${mediumLogo}"  x="140" y="5" height="14" />
+
+   <text transform="translate(100,0)" xlink:href="#blogDate"  x="-90" y="16"  class="smallTitle bold">
+${blogDate}
+   </text>
+    <foreignObject
+      x="10" y="245" width="170" height="200">
+      <div
+        xmlns="http://www.w3.org/1999/xhtml"
+        xlink:href="#blogName"
+          class="title"
+          style="color:#2f80ed;font-size:12px"
+        >${data.title}
+        </div>
+      </foreignObject>
+    <text transform="translate(100,0)" xlink:href="#blogDate" x="-90" y="242" class="smallTitle bold">
+      ${data.user.name}
+    </text>
+   </a >
+  </svg >
+    `;
+};
+
+
+
+
+const DblogCardV = async (data) => {
+    const blogImage = await getBase64(data.cover_image);
+    const mediumLogo = await getBase64("https://raw.githubusercontent.com/Souravdey777/Github-Cards-External-Blogs/master/mediumlogo.png");
+    const blogDate = new Date(data.published_at).toLocaleString('default', { year: 'numeric', month: 'short', day: 'numeric' })
+    const blogLink = data.url;
+
+    return `
+  <svg height="160px" width="400">
+  <style>
+  .title {
+    font: 600 10px 'Segoe UI', Ubuntu, Sans-Serif;
+    fill: #2f80ed;
+    animation: fadeInAnimation 0.8s ease-in-out forwards;
+  }
+
+  .smallTitle {
+    font: 600 10px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif; fill: #333;
+  }
+
+  .bold {
+    font - weight: 700 
+  }
+</style>
+    <a href="${blogLink}" target="_blank">
+      <rect
+        data-testid="card-bg"
+        x="0.5"
+        y="0.5"
+        rx="4.5"
+        height="95%"
+        stroke="#E4E2E2"
+        width="95%"
+        fill="#fffefe"
+        stroke-opacity="1"
+      />
+      <rect
+        height="130"
+        stroke="#E4E2E2"
+        width="50%"
+        fill="#EFEFEF" x="10" y="10"
+        stroke-opacity="1" rx="4.5"
+      />
+      <image 
+        xlink:href="data:image/png;base64,${blogImage}"  
+        x="10"
+        y="10"
+        width="50%"
+        height="130"
+      />
+      <image
+        xlink:href="data:image/png;base64,${mediumLogo}" 
+        x="330"
+        y="10"
+        height="14"
+      />
+
+      <text 
+        transform="translate(100,0)" 
+        xlink:href="#blogDate"
+        x="120"
+        y="20"
+        class="smallTitle bold"
+      >
+      ${blogDate}
+      </text>
+      <foreignObject
+      x="220" y="60" width="150" height="120">
+      <div
+        xmlns="http://www.w3.org/1999/xhtml"
+        xlink:href="#blogName"
+        class="title"
+        style="color:#2f80ed;font-size:13px"
+      >${data.title}
+        </div>
+      </foreignObject>
+      <text transform="translate(100,0)" 
+      xlink:href="#blogDate" x="120" y="45" class="smallTitle bold">
+      ${data.user.name}
+    </text>
+   </a >
+  </svg >
+    `;
+};
+
+module.exports = { DblogCardH, DblogCardV };
--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var app = express();
 app.use(express.json());
 const axios = require('axios');
 const mediumURL = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@"
+const devURL = "https://dev.to/api/articles?username="
 const { blogCardH, blogCardV } = require('./MediumblogCard');
+const { DblogCardH, DblogCardV } = require('./DevblogCard');
 
-const getUserData = async (username) => {
+const getMediumData = async (username) => {
   try {
     const result = await axios.get(mediumURL + username)
     const filteredResult = result.data.items.filter(function (item) {
@@ -15,6 +17,23 @@ const getUserData = async (username) => {
         return false
       }
     });
+    return filteredResult;
+  } catch (error) {
+    console.error(error);
+    return error
+  }
+}
+const getDevData = async (username) => {
+  try {
+    const result = await axios.get(devURL + username)
+    const filteredResult = result.data
+    // .items.filter(function (item) {
+    //   if (!item.thumbnail.includes('stat?event') || !item.thumbnail.includes('&referrerSource')) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // });
     return filteredResult;
   } catch (error) {
     console.error(error);
@@ -36,7 +55,7 @@ app.get('/getMediumBlogs', async (request, response) => {
       return;
     }
     const username = request.query.username;
-    let limit = 10;
+    let limit = 5;
     let type = 'vertical'
     if (request.query.type) {
       type = request.query.type;
@@ -44,7 +63,7 @@ app.get('/getMediumBlogs', async (request, response) => {
     if (request.query.limit) {
       limit = request.query.limit;
     }
-    const resultData = (await getUserData(username));
+    const resultData = (await getMediumData(username));
     let result = `<svg>`;
     if (type === 'horizontal') {
       result = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${resultData.length * 200}" version="1.2" height="310">`;
@@ -75,8 +94,55 @@ app.get('/getMediumBlogs', async (request, response) => {
   }
 });
 
+app.get('/getDevBlogs', async (request, response) => {
+  try {
+    if (!request.query.username) {
+      response.write(JSON.stringify({ error: 'your medium username is require in the query string' }));
+      response.end();
+      return;
+    }
+    const username = request.query.username;
+    let limit = 5;
+    let type = 'vertical'
+    if (request.query.type) {
+      type = request.query.type;
+    }
+    if (request.query.limit) {
+      limit = request.query.limit;
+    }
+    const resultData = (await getDevData(username));
+    let result = `<svg>`;
+    if (type === 'horizontal') {
+      result = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${resultData.length * 200}" version="1.2" height="310">`;
+      await asyncForEach(resultData, async (blog, index) => {
+        if (index >= limit) {
+          return;
+        }
+        const blogCardObj = await DblogCardH(blog);
+        result += `<g transform="translate(${index * 200}, 0)">${blogCardObj}</g>`;
+      });
+    } else {
+      result = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="400" version="1.2" height="${resultData.length * 160}">`;
+      await asyncForEach(resultData, async (blog, index) => {
+        if (index >= limit) {
+          return;
+        }
+        const blogCardObj = await DblogCardV(blog);
+        result += `<g transform="translate(0, ${index * 160})">${blogCardObj}</g>`;
+      });
+    }
+    result += `</svg>`;
+    response.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+    response.write(result);
+    response.end();
+  } catch (error) {
+    console.log(error);
+    response.send('Error while fetching the data' + error);
+  }
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log('Server listening' + port);
+  console.log('Server listening ' + port);
 }); 
