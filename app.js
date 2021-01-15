051f7f754bfef7815042ee1bd7ca4e7d55447d8a
Souravdey777 <piyush.kolkata@gmail.com>
2021-01-16 01:28:35 +0530

    single blog option added

--- app.js
+++ app.js
@@ -141,6 +141,55 @@ app.get('/getDevBlogs', async (request, response) => {
   }
 });
 
+app.get('/getMediumBlogsByID', async (request, response) => {
+  try {
+    if (!request.query.username) {
+      response.write(JSON.stringify({ error: 'your medium username is require in the query string' }));
+      response.end();
+      return;
+    }
+    const username = request.query.username;
+    let limit = 5;
+    let type = 'vertical'
+    if (request.query.type) {
+      type = request.query.type;
+    }
+    if (request.query.limit) {
+      limit = request.query.limit;
+    }
+    let data = (await getMediumData(username));
+    const resultData = [];
+    resultData.push(data[request.query.id])
+    let result = `<svg>`;
+    if (type === 'horizontal') {
+      result = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${resultData.length * 200}" version="1.2" height="310">`;
+      await asyncForEach(resultData, async (blog, index) => {
+        if (index >= limit) {
+          return;
+        }
+        const blogCardObj = await blogCardH(blog);
+        result += `<g transform="translate(${index * 200}, 0)">${blogCardObj}</g>`;
+      });
+    } else {
+      result = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="400" version="1.2" height="${resultData.length * 160}">`;
+      await asyncForEach(resultData, async (blog, index) => {
+        if (index >= limit) {
+          return;
+        }
+        const blogCardObj = await blogCardV(blog);
+        result += `<g transform="translate(0, ${index * 160})">${blogCardObj}</g>`;
+      });
+    }
+    result += `</svg>`;
+    response.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+    response.write(result);
+    response.end();
+  } catch (error) {
+    console.log(error);
+    response.send('Error while fetching the data' + error);
+  }
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
